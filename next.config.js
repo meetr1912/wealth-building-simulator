0a698f6b676f9e9b6165753dad09f0025959bd86
meetr1912 <meetr1912@gmail.com>
2024-12-30 19:52:35 -0400

    Update Next.js configuration to enhance image handling and base path support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   eslint: {
     ignoreDuringBuilds: true,
   },
